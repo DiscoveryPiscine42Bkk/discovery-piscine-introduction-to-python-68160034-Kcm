08fbf71aa2f9e4cdf1eb9d5455832d7eab078c24
68160034-Kcm <68160034@kmitl.ac.th>
2025-11-07 01:49:26 +0000

    msg

---
+++ cell01/ex00/name.py
@@ -0,0 +1,4 @@
+first_name = "Karin"
+last_name = "Chaisongmueang"
+
+print(first_name, last_name)  
---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+last_name = "Karin"  
+first_name = "Chaisongmueang"
+whole_name = last_name + first_name
+print(whole_name)
